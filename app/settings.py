fe0f82ecb35b60b43881a4d63d661dd3d2e079a7
Alexey K <uuuuiiiaaa@yandex.ru>
2020-10-12 18:12:14 +0300

    update settings

--- app/settings.py
+++ app/settings.py
@@ -19,14 +19,13 @@ class Settings(BaseSettings):
     ENV: Env = Env.LOCAL
     REDIS_HOST: str = 'redis://redis:6379/1'
     BASE_DIR: Path = Path().cwd()
-    API: str = 'https://api.finex-etf.ru/v1'
     SENTRY_DSN: Optional[str] = None
 
     # Pg дефолтные значения нужны для работы alembic
     PG_HOST: str = 'localhost'
-    PG_DB: str = 'calc_db_robo'
-    PG_USER: str = 'calc_db_robo'
-    PG_PASSWORD: str = 'calc_db_robo'
+    PG_DB: str = 'postgres'
+    PG_USER: str = 'postgres'
+    PG_PASSWORD: str = 'postgres'
     PG_PORT: int = 5432
 
     OPENAPI_URL: str = '/openapi.json'
