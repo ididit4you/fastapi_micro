16172902093190250f4d789dc8aeb30f23e73333
Alexey K <uuuuiiiaaa@yandex.ru>
2020-10-12 21:26:36 +0300

    Add orjson response to records

--- api/records/views.py
+++ api/records/views.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 
 from fastapi import APIRouter, Request
+from fastapi.responses import ORJSONResponse
 
 from api.records.models import record
 from api.records.shema import Record
@@ -8,7 +9,13 @@ from api.records.shema import Record
 router = APIRouter()
 
 
-@router.get('/records/', summary='Records list', name='records', response_model=List[Record])
+@router.get(
+    '/records',
+    summary='Records list',
+    name='records',
+    response_class=ORJSONResponse,
+    response_model=List[Record],
+)
 async def create_item(request: Request) -> Any:
     """Fetch all records."""
     db = request.app.state.db
