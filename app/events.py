6dcf7e23d7206ef76173c7b2babdfe5f3673bb3e
Alexey K <uuuuiiiaaa@yandex.ru>
2020-10-12 20:13:01 +0300

    Create startup and shutdown event handlers

---
+++ app/events.py
@@ -0,0 +1,25 @@
+
+from typing import Callable, Coroutine
+from fastapi import FastAPI
+
+from database import connect_db, disconnect_db, connect_redis, disconnect_redis
+
+EventHandlerType = Callable[[], Coroutine[None, None, None]]
+
+
+def startup_events(app: FastAPI) -> EventHandlerType:
+    """Init connections e.t.c."""
+    async def startup() -> None:  # noqa: WPS430
+        """List startup events here."""
+        await connect_db(app=app)
+        await connect_redis(app=app)
+    return startup
+
+
+def shutdown_events(app: FastAPI) -> EventHandlerType:
+    """Drop connections e.t.c."""
+    async def shutdown() -> None:  # noqa: WPS430
+        """List shutdown events here."""
+        await disconnect_db(app=app)
+        await disconnect_redis(app=app)
+    return shutdown
