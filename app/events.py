ae5c39d684b12a8b5188d52d0ab5b8fec10f5d6e
Alexey K <uuuuiiiaaa@yandex.ru>
2020-12-02 18:01:17 +0300

    Remove unused stuff

--- app/events.py
+++ app/events.py
@@ -3,9 +3,6 @@ from typing import Callable, Coroutine
 
 from fastapi import FastAPI
 
-from app.db_conn import connect_db, disconnect_db
-from app.redis_conn import connect_redis, disconnect_redis
-
 EventHandlerType = Callable[[], Coroutine[None, None, None]]
 
 
@@ -13,8 +10,7 @@ def startup_events(app: FastAPI) -> EventHandlerType:
     """Init connections e.t.c."""
     async def startup() -> None:  # noqa: WPS430
         """List startup events here."""
-        await connect_db(app=app)
-        await connect_redis(app=app)
+        pass
     return startup
 
 
@@ -22,6 +18,5 @@ def shutdown_events(app: FastAPI) -> EventHandlerType:
     """Drop connections e.t.c."""
     async def shutdown() -> None:  # noqa: WPS430
         """List shutdown events here."""
-        await disconnect_db(app=app)
-        await disconnect_redis(app=app)
+        pass
     return shutdown
--- app/models.py
+++
@@ -1,4 +0,0 @@
-"""Collect all meta here for import in migrations.env.py."""
-from api.calc.models import metadata as calc_meta
-
-metadata = [calc_meta]
