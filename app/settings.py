2b8f215e24b751eccdc16024c1d2057dc57a5b71
Alexey K <uuuuiiiaaa@yandex.ru>
2021-01-26 19:30:03 +0300

    fix: No optional redis settings
    If you dont need redis remove it

--- app/settings.py
+++ app/settings.py
@@ -9,26 +9,26 @@ DEFAULT_POSTGRES_PORT = '5432'
 class RedisSettings(BaseSettings):
     """Setup redis."""
 
-    REDIS_HOST: Optional[str] = None
+    REDIS_HOST: str = 'localhost'
     REDIS_USER: Optional[str] = None
-    REDIS_PORT: Optional[str] = None
+    REDIS_PORT: str = '6379'
     REDIS_PASSWORD: Optional[str] = None
-    REDIS_DB: Optional[str] = None
+    REDIS_DB: str = '/10'
     REDIS_URI: Optional[RedisDsn] = None
 
     @validator('REDIS_URI', pre=True)
     def build_redis_dsn(cls, redis_dsn: Optional[str], values: Dict[str, Any]) -> Any:  # noqa: N805
         """Set REDIS_URI."""
-        if values.get('REDIS_HOST'):
-            return RedisDsn.build(
-                scheme='redis',
-                user=values.get('REDIS_USER'),
-                password=values.get('REDIS_PASSWORD'),
-                host=values.get('REDIS_HOST'),
-                port=values.get('REDIS_PORT'),
-                path=values.get('REDIS_DB'),
-            )
-        return None
+        if redis_dsn:
+            return redis_dsn
+        return RedisDsn.build(
+            scheme='redis',
+            user=values.get('REDIS_USER'),
+            password=values.get('REDIS_PASSWORD'),
+            host=values.get('REDIS_HOST'),
+            port=values.get('REDIS_PORT'),
+            path=values.get('REDIS_DB'),
+        )
 
 
 class DbSettings(BaseSettings):
@@ -40,25 +40,22 @@ class DbSettings(BaseSettings):
     POSTGRES_USER: str = 'postgres'
     POSTGRES_PASSWORD: str = 'postgres'
     POSTGRES_PORT: str = DEFAULT_POSTGRES_PORT
-    POSTGRES_URI: Optional[PostgresDsn]
-    POSTGRES_POOL_MIN: Optional[int] = 10
-    POSTGRES_POOL_MAX: Optional[int] = 20
+    POSTGRES_URI: Optional[PostgresDsn] = None
+    POSTGRES_POOL_MIN: int = 10
+    POSTGRES_POOL_MAX: int = 20
 
     @validator('POSTGRES_URI', pre=True)
-    def build_pg_dsn(cls, val: Optional[str], values: Dict[str, Any]) -> Any:  # noqa: N805
+    def build_pg_dsn(cls, pg_dsn: Optional[str], values: Dict[str, Any]) -> Any:  # noqa: N805
         """Set POSTGRES_URI."""
-        if isinstance(val, str):
-            return val
-        db = f'/{values.get("POSTGRES_DB")}'
-        if db and values.get('ENV') == 'TEST':
-            db = f'{db}_test'
+        if pg_dsn:
+            return pg_dsn
         return PostgresDsn.build(
             scheme='postgresql',
             user=values.get('POSTGRES_USER'),
             password=values.get('POSTGRES_PASSWORD'),
             host=values.get('POSTGRES_HOST'),
             port=values.get('POSTGRES_PORT'),
-            path=db,
+            path='/{db}'.format(db=values.get('POSTGRES_DB')),
         )
 
 
@@ -77,7 +74,7 @@ class Settings(BaseSettings):
     postgres: DbSettings = DbSettings()
 
     @validator('SENTRY_DSN', pre=True)
-    def build_sentry_dsn(cls, sentry_dsn: Optional[str], values: Dict[str, Any]) -> Any:  # noqa: N805
+    def build_sentry_dsn(cls, sentry_dsn: Optional[str]) -> Any:  # noqa: N805
         """Set SENTRY_DSN."""
         if sentry_dsn:
             return HttpUrl(sentry_dsn)
