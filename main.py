80910fdea5a4f380ee80e974ae26f0558f06e4fc
Alexey K <uuuuiiiaaa@yandex.ru>
2021-01-26 14:42:22 +0300

    feat: Return sturtapp events back

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 
+from app.events import shutdown_events, startup_events
 from app.routes import main_router
 from app.settings import conf
 
@@ -14,6 +15,10 @@ app = FastAPI(
     # The root_path is used to handle cases when proxy adds an extra path prefix that is not seen by application.
     root_path=conf.ROOT_PATH,
 )
+
+app.add_event_handler('startup', startup_events(app))
+app.add_event_handler('shutdown', shutdown_events(app))
+
 
 # Sentry
 if conf.SENTRY_DSN:
