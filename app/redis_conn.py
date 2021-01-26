e4a44e44ec9d11f78878096d0fa6fae4117e43b8
Alexey K <uuuuiiiaaa@yandex.ru>
2021-01-26 14:41:37 +0300

    feat: Add conditional redis connection

--- app/redis_conn.py
+++ app/redis_conn.py
@@ -7,10 +7,12 @@ from app.settings import conf
 
 async def connect_redis(app: FastAPI) -> None:
     """Add redis connection to app state."""
-    app.state.redis = await aioredis.create_redis_pool(conf.REDIS_HOST)
+    if conf.redis.REDIS_URI:
+        app.state.redis = await aioredis.create_redis_pool(conf.redis.REDIS_URI)
 
 
 async def disconnect_redis(app: FastAPI) -> None:
     """Disconnect redis."""
-    app.state.redis.close()
-    await app.state.redis.wait_closed()
+    if conf.redis.REDIS_URI:
+        app.state.redis.close()
+        await app.state.redis.wait_closed()
