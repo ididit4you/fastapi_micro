5149ab47a0bf1d48c83b09b9d26ae5cbcfc188f0
Alexey K <uuuuiiiaaa@yandex.ru>
2021-02-16 13:49:15 +0300

    feat: Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,10 @@
 from typing import AsyncGenerator, Generator
 
 import databases
-from databases.core import Database
 import pytest
 from alembic import command
 from alembic.config import Config
-from pydantic import PostgresDsn
-from sqlalchemy import create_engine
+from databases.core import Database
 from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from fastapi.testclient import TestClient
@@ -15,32 +13,22 @@ from fastapi.testclient import TestClient
 from app.settings import conf
 from main import app
 
-TEST_DB = PostgresDsn.build(
-    scheme='postgresql',
-    user=conf.postgres.POSTGRES_USER,
-    password=conf.postgres.POSTGRES_PASSWORD,
-    host='localhost',
-    port=conf.postgres.POSTGRES_PORT,
-    path='/{db}.{ending}'.format(
-        db=conf.postgres.POSTGRES_DB,
-        ending='pytest',
-    ),
-)
+TEST_DB = conf.postgres.POSTGRES_URI
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(autouse=True, scope='session')
 def temp_db() -> Generator[None, None, None]:
     """Create new test db for testing session."""
-    create_engine(TEST_DB)
+    assert TEST_DB.endswith('.pytest')
     db_exists = database_exists(TEST_DB)
+
     if db_exists:
         drop_database(TEST_DB)
     create_database(TEST_DB)  # Create the test database.
     config = Config('alembic.ini')  # Run the migrations.
     config.set_main_option('sqlalchemy.url', TEST_DB)
-    assert config.get_main_option('sqlalchemy.url') == TEST_DB
     command.upgrade(config, 'head')
-    yield  # Run the tests.
+    yield  # Run tests.
     drop_database(TEST_DB)
 
 
